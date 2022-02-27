14810dc1af39efa0ee1b4e301971d31f645065ed
be-favorite <favorite@kakao.com>
2022-02-27 18:00:27 +0900

    initial commit of my website

---
+++ Code.R
@@ -0,0 +1,11 @@
+library(distill)
+create_post(
+    "test", # mandatory
+    author = "auto",
+    slug = "auto", # generates a website slug (URL)
+    date_prefix = TRUE, # adds date for sorting
+    draft = FALSE, 
+    edit = interactive()
+)
+
+usethis::use_git()
